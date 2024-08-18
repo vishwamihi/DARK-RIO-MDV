4bfdb6a1efb268c7dc617726774234a9447aafe3
vishwamihi <98463616+vishwamihi@users.noreply.github.com>
2024-08-18 11:01:51 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "DARK-RIO-MD&d1FC7jv2",
 ALIVE_IMG: process.env.ALIVE_IMG || "",
 ALIVE_MSG: process.env.ALIVE_MSG || "HEY I AM DARK-RIO-MD",
 
